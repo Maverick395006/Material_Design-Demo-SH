09b0a3158d4d54699f4de74cd7d809eff6e7f69b
Nikunj India <contact@yamiitsolutions.com>
2023-03-19 20:29:30 +0530

    4.1 Basic Components 4.2 Simple SnackBar 4.3 Action Snackbar Callback 4.4 Floating Action Button 4.5 Floating Action Button 4.6 Coordinator Layout

--- app/src/main/java/com/maverick/materialdeisign_demo_sh/MainActivity.kt
+++ app/src/main/java/com/maverick/materialdeisign_demo_sh/MainActivity.kt
@@ -2,10 +2,57 @@ package com.maverick.materialdeisign_demo_sh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import com.google.android.material.snackbar.Snackbar
+import com.maverick.materialdeisign_demo_sh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.simple_snackbar -> {
+                    showSimpleSnackBar()
+                }
+                R.id.snackbar_action_callback -> {
+                    showSnackBarWithActionCallBack()
+                }
+                R.id.custom_snackbar -> {
+                    showCustomSnackBar()
+                }
+            }
+            true
+        }
+
+        binding.fab.setOnClickListener {
+            Snackbar.make(binding.root, "Floating Button in COL", Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun showSimpleSnackBar() {
+        Snackbar.make(binding.root, "Simple Snack Bar", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun showSnackBarWithActionCallBack() {
+        val snackbar =
+            Snackbar.make(binding.root, "File Deleted Successfully", Snackbar.LENGTH_LONG)
+        snackbar.setAction("UNDO") {
+            Snackbar.make(binding.root, "File Recovered Successfully", Snackbar.LENGTH_LONG).show()
+        }
+        snackbar.show()
+    }
+
+    private fun showCustomSnackBar() {
+
     }
 }
