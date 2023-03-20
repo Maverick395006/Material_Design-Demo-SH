223fe0af99829c6f8978337219ae01959bf8bf1f
Nikunj India <contact@yamiitsolutions.com>
2023-03-20 10:31:02 +0530

    5 Created new Branch [Didn't Practice Yet]

--- app/src/main/java/com/maverick/materialdeisign_demo_sh/MainActivity.kt
+++ app/src/main/java/com/maverick/materialdeisign_demo_sh/MainActivity.kt
@@ -1,30 +1,20 @@
 package com.maverick.materialdeisign_demo_sh
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import com.google.android.material.snackbar.Snackbar
 import com.maverick.materialdeisign_demo_sh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        binding.standAloneToolbar.setOnClickListener {
-            startActivity(Intent(this,StandAloneToolbarActivity::class.java))
-        }
-
-        binding.actionbarToolbar.setOnClickListener {
-            startActivity(Intent(this,ActionBarToolbarActivity::class.java))
-        }
-
-        binding.contextualMenu.setOnClickListener {
-            startActivity(Intent(this,ContextualMenuActivity::class.java))
-        }
+        setContentView(binding.root)
 
     }
 
